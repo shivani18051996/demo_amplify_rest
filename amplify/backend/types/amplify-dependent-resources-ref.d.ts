8287ceaaebded47fd6cf82ac7988b2924cc41d82
shivanitrivedi123 <shivani.trivedi@gtcsys.com>
2024-01-24 18:10:24 +0530

    feat: #  - Add the authentiation in the amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "demoproject21be3ed4d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import "@aws-amplify/ui-react/styles.css";
+import {
+  Button,
+  Flex,
+  Heading,
+  Text,
+  TextField,
+  View,
+  withAuthenticator,
+  Image,
+  Card
+} from "@aws-amplify/ui-react";
+function App({signOut}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload first.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<View className='App'>
+  <Card>
+    <Image src={logo} />
+    <Heading level={1}>We now have Auth!</Heading>
+  </Card>
+  <Button onClick={signOut}>Signout</Button>
+
+</View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
